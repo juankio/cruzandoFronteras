8c4c1e01f620b3c31a6de3757afeb14610f1042e
Miguel <juanmiguelruizsupe@gmail.com>
2024-06-04 18:09:58 -0500

    cambios del footer

--- components/ui/footer.vue
+++ components/ui/footer.vue
@@ -1,30 +1,53 @@
 <template>
     <div>
-        <!-- Footer -->
         <footer
             class="bg-gradient-to-r from-yellow-500 to-yellow-300 dark:from-yellow-200 dark:to-yellow-300 text-black py-8 mt-8">
-            <div class="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
-                <div>
-                    <p class="font-bold mb-2">Corporación Grupo Luna</p>
+            <div
+                class="container mx-auto px-4 flex flex-col items-center md:flex-row md:justify-between space-y-4 md:space-y-0">
+                <div class="text-center md:text-left">
+                    <p class="font-bold mb-2">Fundación Cruzando Fronteras</p>
                     <p>Calle 14 Sur 48B - 61 B/Serramonte 1 Ca 6A</p>
                     <p>Villavicencio, Meta</p>
                     <p>Colombia</p>
                 </div>
-                <div class="mt-4 md:mt-0">
+                <div class="text-center md:text-left">
                     <p class="font-bold mb-2">Contact Information</p>
                     <p><a href="tel:+573167641291">+57 (316) 764-1291</a></p>
-                    <p><a href="mailto:corporaciongrupoluna@gmail.com">corporaciongrupoluna@gmail.com</a></p>
-                    <p><a href="https://www.corporaciongrupoluna.com" target="_blank">www.corporaciongrupoluna.com</a>
+                    <p><a href="mailto:FundaciónCruzandoFronteras@gmail.com">FundaciónCruzandoFronteras@gmail.com</a>
                     </p>
+                    <p><a href="https://www.FundaciónCruzandoFronteras.com"
+                            target="_blank">www.FundaciónCruzandoFronteras.com</a></p>
                 </div>
-                <div class="mt-4 md:mt-0">
+                <div class="text-center md:text-left">
                     <p class="font-bold mb-2">Follow Us</p>
-                    <div class="flex space-x-4">
+                    <div class="flex justify-center md:justify-start space-x-4">
                         <a href="#" class="text-black">
-                            <UIcon name="i-heroicons-light-bulb" />
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
+                                class="svg-inline--fa fa-facebook-f fa-w-10" role="img"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                <path fill="currentColor"
+                                    d="M279.14 288l14.22-92.66h-88.91V132.41c0-25.35 12.42-50.06 52.24-50.06H293V6.26S266.27 0 240.49 0C166.86 0 121 42.85 121 120.36v75H39v92.66h82v223.68A319.22 319.22 0 0 0 196.68 512V288z">
+                                </path>
+                            </svg>
                         </a>
-                        <a href="#" class="text-black"><i class="fab fa-twitter"></i></a>
-                        <a href="#" class="text-black"><i class="fab fa-youtube"></i></a>
+                        <a href="#" class="text-black">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
+                                class="svg-inline--fa fa-twitter fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512">
+                                <path fill="currentColor"
+                                    d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558A296.509 296.509 0 0 1 0 407.337a209.073 209.073 0 0 0 24.231 1.241 209.289 209.289 0 0 0 130.127-44.973 104.671 104.671 0 0 1-97.81-72.636 132.163 132.163 0 0 0 19.588 1.883c9.716 0 19.42-1.241 28.491-3.609a104.625 104.625 0 0 1-83.94-102.651v-1.318a105.34 105.34 0 0 0 47.206 13.203 104.58 104.58 0 0 1-32.341-139.878 297.614 297.614 0 0 0 216.366 109.947 118.34 118.34 0 0 1-2.564-23.877 104.625 104.625 0 0 1 180.706-71.564 207.17 207.17 0 0 0 66.359-25.297 105.232 105.232 0 0 1-45.981 58.064 208.657 208.657 0 0 0 60.309-16.409 222.272 222.272 0 0 1-52.597 54.536z">
+                                </path>
+                            </svg>
+                        </a>
+                        <a href="#" class="text-black">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube"
+                                class="svg-inline--fa fa-youtube fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 576 512">
+                                <path fill="currentColor"
+                                    d="M549.655 124.083c-6.281-23.065-24.675-41.383-47.8-47.671C456.479 64 288 64 288 64S119.521 64 74.145 76.412c-23.125 6.288-41.519 24.606-47.8 47.671-12.412 45.422-12.412 140.484-12.412 140.484s0 95.061 12.412 140.484c6.281 23.065 24.675 41.383 47.8 47.671 45.376 12.412 213.855 12.412 213.855 12.412s168.479 0 213.855-12.412c23.125-6.288 41.519-24.606 47.8-47.671 12.412-45.422 12.412-140.484 12.412-140.484s0-95.061-12.412-140.484zm-317.246 213.835V174.082l142.812 81.918-142.812 81.918z">
+                                </path>
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -33,29 +56,6 @@
 </template>
 
 <script setup lang="ts">
-const route = useRoute()
-const colorMode = useColorMode()
-const isOpen = ref(false)
-
-const isDark = computed({
-    get() {
-        return colorMode.value === 'dark'
-    },
-    set(value: any) {
-        colorMode.preference = value ? 'dark' : 'light'
-    }
-})
-
-const linkClass = (path: string) => {
-    return [
-        'block py-2 px-3 rounded hover:bg-yellow-400 dark:hover:bg-transparent md:hover:bg-transparent md:border-0 md:hover:font-bold md:p-0 text-black dark:text-black transition duration-300 ease-in-out ',
-        { 'border-b-4 border-orange-500 dark:border-orange-400': route.path === path }
-    ]
-}
-
-const toggleDarkMode = () => {
-    isDark.value = !isDark.value
-}
 </script>
 
 <style scoped>
