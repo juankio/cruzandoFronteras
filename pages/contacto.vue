9877ff03c7284e2e77bd8e181e0a3f386de2fd3c
Miguel <juanmiguelruizsupe@gmail.com>
2024-06-14 18:20:11 -0500

    padigg de contactanos

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2
-            class="text-4xl md:text-6xl text-center font-bold bg-gradient-to-r from-orange-500 via-yellow-400 to-orange-500 text-transparent bg-clip-text animate-shine">
+            class="text-4xl md:text-6xl text-center font-bold bg-gradient-to-r from-orange-500 via-yellow-400 to-orange-500 text-transparent bg-clip-text animate-shine pb-10">
             Contáctanos
         </h2>
         <div>
