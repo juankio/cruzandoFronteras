cdc8dba4f82567ece538346225742d4f5bdd141a
Miguel <juanmiguelruizsupe@gmail.com>
2024-06-15 15:23:04 -0500

    prueba de verificacion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,8 @@ export default defineNuxtConfig({
     head: {
       title: 'Fundación Cruzando Fronteras',
       meta: [
-        { name: 'description', content: 'Fundación Cruzando Fronteras ofrece servicios de educación, recreación, bienestar, nutrición y salud a comunidades vulnerables en Colombia, promoviendo la convivencia ciudadana y el progreso comunitario.' }
+        { name: 'description', content: 'Fundación Cruzando Fronteras ofrece servicios de educación, recreación, bienestar, nutrición y salud a comunidades vulnerables en Colombia, promoviendo la convivencia ciudadana y el progreso comunitario.' },
+        { name: 'google-site-verification', content: '1dTiHSyCEI2qTa3FMaR2x1a5ZAWEx6OHZvNC6dBTwZo' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/s.png' }
